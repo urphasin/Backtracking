a06dee2605d8eeb6fc27df87f9ee3d550909ea87
Otito Amuga <urphasin@gmail.com>
2026-02-03 00:55:29 -0700

    cold.exe

---
+++ src/STRUCTURES/TreeNode.java
@@ -0,0 +1,20 @@
+package STRUCTURES;
+
+public class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int value;
+
+    public TreeNode(int value) {
+        this.left = null;
+        this.right= null;
+        this.value = value;
+    }
+    public TreeNode() {
+        this.left = null;
+        this.right= null;
+        this.value = 0;
+    }
+
+
+}
